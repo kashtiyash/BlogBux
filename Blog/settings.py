cd76ef87c425256ea1fbce8b7b16e9c59dab3082
Zeus <79009766+Zenerproton@users.noreply.github.com>
2022-06-01 19:35:53 +0530

    3.1 commit

--- Blog/settings.py
+++ Blog/settings.py
@@ -114,7 +114,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+LOGIN_REDIRECT_URL = "Home"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_view
 urlpatterns = [
     path('signup/', views.signup, name="users-sign-up"),
+    path('login/', auth_view.LoginView.as_view(template_name="users/login.html"), name="users-login"),
+    path('logout/', auth_view.LogoutView.as_view(template_name="users/logout.html"), name="users-logout"),
+
 
 ]
